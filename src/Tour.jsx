f3a86b94a1c4a673e7d28e28302d8c27f52f7371
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-04-28 13:14:06 +0500

    Display image

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,10 +1,21 @@
-const Tour = (props) => {
-  const tourTwo = props.tourOne;
-  console.log(tourTwo);
+import "./Tours"
+import './index.css'
+
+
+const Tour = ({id, image, info, price, name}) => {
+  
   return (
-    <div key={props.tourOne.id}>
-      <h1>{tourTwo[1].name}</h1>
-    </div>
+   <article className="single-tour">
+    <img src={image} alt={name} />
+    <footer>
+      <div className="info-btn">
+        <h4>{name}</h4>
+        <h4 className="tour-price">${price}</h4>
+      </div>
+      <p>{info}</p>
+      <button className="delete-btn">Not Intrested</button>
+    </footer>
+   </article>
   )
 }
 export default Tour
--- src/Tours.jsx
+++ src/Tours.jsx
@@ -1,13 +1,24 @@
 import Tour from "./Tour"
+import './index.css'
 
-const Tours = (props) => {
+const Tours = ({tour}) => {
 
-  const newTour = props.tour;
+  const newTour = tour;
 
   return (
-    <div >
-    <Tour tourOne={newTour}/>
+    <section >
+    <div className="title">
+      <h2>Ours Tours</h2>
+      <div className="underline"></div>
     </div>
+    <div>
+      {tour.map((tu) =>{
+        return <Tour key={tu.id} {...tu}>
+
+        </Tour>
+      })}
+    </div>
+    </section>
   )
 }
 export default Tours
